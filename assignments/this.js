b9db34181828196b91ee4ed5ecf760558b861bd5
sandyjernigan <sandy.jernigan@gmail.com>
2019-04-22 22:55:59 -0400

    The four principles of "this", Principle 1 & 2

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,34 @@
-/* The for principles of "this";
+/* The four principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* This is a placeholder (pronoun) to use for an object. 
+* 1. Principle 1: Window/Global Object Binding - This refers to the global or window object.
+* 2. Principle 2: Implicit Binding - When using a function or method the object left of the dot is 'this'. Allowing you to pass objects.
+* 3. Principle 3: New binding - This refers to the object that will be created.
+* 4. Principle 4: Explicit binding - Using Javascripts .call or .apply will explicitly bind using this.
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// Principle 1 - Window Binding
 
-// code example for Window Binding
+    console.log(this === window); //window defined in browser
+    function cb() {
+        console.log(this);
+    } // this refers to the window/global object
+    cb();
 
-// Principle 2
+// Principle 2 - Implicit Binding
 
-// code example for Implicit Binding
+    const myObject = {
+        name: "John",
+        sayHello: function(name) {
+            console.log(`Hello, my name is ${this.name}.`);
+        } // this refers to the object
+    }
+    myObject.sayHello();
 
-// Principle 3
+// Principle 3 - New Binding
 
-// code example for New Binding
+// Principle 4 - Explicit Binding
 
-// Principle 4
-
-// code example for Explicit Binding
