83e47e51ce774f84c311d9047ed4ab16d051c60e
sandyjernigan <sandy.jernigan@gmail.com>
2019-04-23 20:21:19 -0400

    GameObject

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -16,9 +16,19 @@
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
 
+  function GameObject(attributes){
+    this.createdAt = attributes.createdAt,
+    this.name = attributes.name,
+    this.dimensions = attributes.dimensions,
+  }
+
+  GameObject.prototype.destroy = function() {
+    return `${this.name} was removed from the game.`
+  }
+
 /*
   === CharacterStats ===
-  * healthPoints
+  * healthPoints++
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
@@ -32,6 +42,16 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
+
+/*
+  dimensions: {
+    length: 2,
+    width: 1,
+    height: 1,
+  },
+  healthPoints: 5,
+  name: 'Bruce',
+*/
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +61,6 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +121,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
